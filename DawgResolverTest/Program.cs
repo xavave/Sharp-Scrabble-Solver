ea5a2679f78e092ae079b556d7d6bf8f951b3cbf
xaverbouch <xavier.averbouch_prestataire@spedidam.fr>
2019-12-04 11:45:36 +0100

    update test

--- DawgResolverTest/Program.cs
+++ DawgResolverTest/Program.cs
@@ -14,9 +14,9 @@ namespace DawgResolverTest
 
             g.Grid[(int)Game.BoardSize / 2, (int)Game.BoardSize / 2].Letter = g.Alphabet.Find(c => c.Char == 'A');
             g.Grid[8, 7].Letter = g.Alphabet.Find(c => c.Char == 'N');
-            //g.Grid[9, 7].Letter = g.Alphabet.Find(c => c.Char == 'N');
-            //g.Grid[10, 7].Letter = g.Alphabet.Find(c => c.Char == 'E');
-            //g.Grid[11, 7].Letter = g.Alphabet.Find(c => c.Char == 'E');
+            g.Grid[9, 7].Letter = g.Alphabet.Find(c => c.Char == 'N');
+            g.Grid[10, 7].Letter = g.Alphabet.Find(c => c.Char == 'E');
+            g.Grid[11, 7].Letter = g.Alphabet.Find(c => c.Char == 'E');
             //g.Grid[(int)Game.BoardSize / 2, (int)Game.BoardSize / 2].Letter = g.Alphabet.Find(c => c.Char == 'A');
             //g.Grid[7, 8].Letter = g.Alphabet.Find(c => c.Char == 'N');
             //g.Grid[7, 9].Letter = g.Alphabet.Find(c => c.Char == 'N');
