61709c4315dfc3b2db1d6b4669432cf247a1f44a
Xavier Averbouch <xavieraverbouch@gmail.com>
2020-01-22 16:56:15 +0100

    Add files via upload

--- Dawg.Resolver.WPF/MainWindow.xaml.cs
+++ Dawg.Resolver.WPF/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using DawgResolver;
-using System;
-using System.Collections.Generic;
+using DawgResolver.Model;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DawgResolver.Resolver.WPF
 {
@@ -24,16 +12,16 @@ namespace DawgResolver.Resolver.WPF
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private Word selectedHintWord;
-        private Tile[,] data;
+        private VTile[,] data;
         public Game game { get; set; }
         public MainWindow()
         {
-            game = new Game();
+            game = new Game(Dictionnaire.NomDicoDawgODS7);
             InitializeComponent();
             game.InitBoard();
 
         }
-        public Tile[,] Data
+        public VTile[,] Data
         {
             get => this.data;
 
@@ -51,7 +39,7 @@ namespace DawgResolver.Resolver.WPF
                 if (value != null && selectedHintWord != value)
                 {
                     selectedHintWord = value;
-                    selectedHintWord.StartTile.SetWord(game, selectedHintWord.Text, selectedHintWord.Direction);
+                    selectedHintWord.StartTile.SetWord(new Player(game, "player 1"), selectedHintWord.Text, selectedHintWord.Direction);
                     UpdateData();
                 }
             }
@@ -78,13 +66,13 @@ namespace DawgResolver.Resolver.WPF
         }
         private void OnNewGame(object sender, RoutedEventArgs e)
         {
-            game = new Game();
+            game = new Game(Dictionnaire.NomDicoDawgODS7);
             game.InitBoard();
         }
 
         private void btnHint_Click(object sender, RoutedEventArgs e)
         {
-            game.Resolver.NewDraught(game.Player1, txtRack.Text);
+            //game.Resolver.NewDraught(game.Player1, txtRack.Text);
             lstHint.ItemsSource = game.Resolver.FindMoves(game.Player1);
         }
     }
